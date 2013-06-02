bd92c714f1f22765a8f2f47f49d64abc9ddb4a15
David Gleich <dgleich@purdue.edu>
2013-06-02 09:18:32 -0700

    Working prediction code

--- src/mr_predict2_hadoop.py
+++ src/mr_predict2_hadoop.py
@@ -66,25 +66,36 @@ class MRPredictwithSVD(MRJob):
         numNodes = nend - nbegin
         numParasubset = value.size / numNodes
         #print >>sys.stderr, numNodes, numParasubset #should be 33153, 16
+             
         
         for i, weight in enumerate(W):
             K = weight[0] # number of left singular vectors to compute the interpolation
-            for k in range(numNodes):
-                val = 0.
-                err = 0.
-                for j in range(K):
-                    val += value[k][j]*weight[j+1]
-                    #print >>sys.stderr, j
-                for j in range (K,numParasubset):
-                    err += (value[k][j]*weight[j+1])*(value[k][j]*weight[j+1])
-                    #print >>sys.stderr, j
+            interpval = dot(value[:,0:K],weight[1:(K+1)])
+            interperr = dot(value[:,K:]**2,weight[(K+1):]**2)
+            if nbegin==0:
+               
+                print >>sys.stderr, "node=0  ", interpval[0], interperr[0]
+                print >>sys.stderr, value[0,0:K]
+                print >>sys.stderr, value[0,0:K]*weight[1:(K+1)]
+                print >>sys.stderr, weight[1:(K+1)] 
+                print >>sys.stderr, dot(value[0,0:K],weight[1:(K+1)])
+                print >>sys.stderr, "reporter:counter:program,firstrow,1"
+            # for k in range(numNodes):
+            #     val = 0.
+            #     err = 0.
+            #     for j in range(K):
+            #         val += value[k][j]*weight[j+1]
+            #         #print >>sys.stderr, j
+            #     for j in range (K,numParasubset):
+            #         err += (value[k][j]*weight[j+1])*(value[k][j]*weight[j+1])
+            #         #print >>sys.stderr, j
                     
-                yield (r,t,subset[i]), (k+nbegin, val, err)
+            yield (r,t,subset[i]), (noderange, interpval, interperr)
                 
         
     def reducer(self, key, values):
         """
-        input: (r,t,p), (node, val, err)
+        input: (r,t,p), (noderange, val, err)
         output:(r,t,p), (valarray, errarray)
 
         2013-04-29
@@ -93,26 +104,43 @@ class MRPredictwithSVD(MRJob):
         node id explicitly anymore and can just store an array instead.
 
         """
+        print >>sys.stderr, "reducer for key", key
+        myvals = [ val for val in values ] # realize the values to cache them
+        print >>sys.stderr, "reducer for key", key, "values = ", len(myvals)
+        nnodes = max( val[0][1] for val in myvals ) # find the number of nodes
+        print >>sys.stderr, "nnodes = %i"%(nnodes)
+        for val in myvals:
+            print >>sys.stderr, "noderange=", val[0]
+        interpval = zeros ( nnodes )
         
-        val_order = {}
-        err_order = {}
+        interperr = zeros ( nnodes )
+
+        for val in myvals:
+            # val = (noderange, interpval in range, interperr in range)
+            noderange = val[0]
+            interpval[noderange[0]:noderange[1]] = val[1]
+            interperr[noderange[0]:noderange[1]] = val[2]
+        yield key, (interpval, interperr)
+
+        #val_order = {}
+        #err_order = {}
         
-        for i, value in enumerate(values):
-            val_order[value[0]]=value[1]
-            err_order[value[0]]=value[2]
+        #for i, value in enumerate(values):
+        #    val_order[value[0]]=value[1]
+        #    err_order[value[0]]=value[2]
             
-        val = [ ]  
-        for k,value in sorted(val_order.iteritems()):
-            val.append(value)
-        val = array(val)
+        #val = [ ]  
+        #for k,value in sorted(val_order.iteritems()):
+        #    val.append(value)
+        #val = array(val)
         
-        err = [ ]  
-        for k,value in sorted(err_order.iteritems()):
-            err.append(value)
-        err = array(err)
+        #err = [ ]  
+        #for k,value in sorted(err_order.iteritems()):
+        #    err.append(value)
+        #err = array(err)
         
         #print >>sys.stderr, key,val
-        yield key, (val, err)
+        #yield key, (val, err)
 
 if __name__ == '__main__':
     MRPredictwithSVD.run()
